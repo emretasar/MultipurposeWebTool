789346d937e45feffb21cb9bf80121db75538d84
Emre Taşar <tasaremre7@gmail.com>
2024-11-12 20:51:24 +0300

    getting currency rates from evds api

--- assets/forms.py
+++ assets/forms.py
@@ -6,8 +6,8 @@ class CurrencyAssetForm(forms.ModelForm):
     date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
     currency_type = forms.Select(choices=CurrencyAssetEntry.CURRENCY_CHOICES)
     amount = forms.DecimalField(max_digits=12, decimal_places=2)
-    exchange_rate = forms.DecimalField(max_digits=10, decimal_places=4, initial = Decimal('1.00'))
+    # exchange_rate = forms.DecimalField(max_digits=10, decimal_places=4, initial = Decimal('1.00'))
 
     class Meta:
         model = CurrencyAssetEntry
-        fields = ['date', 'currency_type', 'amount', 'exchange_rate']
+        fields = ['date', 'currency_type', 'amount']
