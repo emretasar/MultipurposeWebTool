b02b7955973d3208f7fb6ba7ec92204fafe2c407
Emre Taşar <tasaremre7@gmail.com>
2024-12-03 19:28:04 +0300

    pie chart display added

--- assets/urls.py
+++ assets/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
-from .views import currency_asset_view
-from .views import currency_line_chart
+from .views import currency_line_chart, distribution_pie_chart
 
 
 urlpatterns = [
     path('', views.currency_asset_view, name='assets_page'),
 	path('update_asset/<str:pk>/', views.update_entry, name="update_asset_entry"),
 	path('delete_asset/<str:pk>/', views.delete_entry, name="delete_asset_entry"),
-    path('chart/<str:currency_type>/', currency_line_chart, name='currency_line_chart')
+    path('chart/<str:currency_type>/', currency_line_chart, name='currency_line_chart'),
+    path('pie/distribution/', distribution_pie_chart, name='distribution_pie_chart'),
 ]
--- assets/views.py
+++ assets/views.py
@@ -72,6 +72,40 @@ def currency_line_chart(request, currency_type):
     context = {'chart': image_base64, 'currency_type': currency_type}
     return render(request, 'currency_chart.html', context)
 
+def distribution_pie_chart(request):
+    matplotlib.use('Agg')  # Use Agg backend for rendering without GUI
+
+    latest_entries = (
+        CurrencyAssetEntry.objects
+        .values('currency_type')           
+        .annotate(latest_date=Max('date'))  
+    )
+
+    latest_assets = CurrencyAssetEntry.objects.filter(
+        date__in=[entry['latest_date'] for entry in latest_entries]
+    )
+
+    currency_types = [entry.currency_type for entry in latest_assets]
+    amounts = [entry.amount for entry in latest_assets]
+    exchange_rates = [entry.exchange_rate for entry in latest_assets]
+    date = latest_assets[0].date
+
+    currency_to_trys = []
+    for idx, amount in enumerate(amounts):
+        currency_to_trys.append(float(amount) * float(exchange_rates[idx]))
+
+    fig, ax = plt.subplots()
+    ax.pie(currency_to_trys, labels=currency_types, autopct='%1.1f%%',
+    pctdistance=1.25, labeldistance=.6)
+
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+    image_base64 = base64.b64encode(buf.read()).decode('utf-8')
+    buf.close()
+
+    context = {'chart': image_base64, 'date': date}
+    return render(request, 'distribution_pie.html', context)
 
 def update_entry(request, pk):
     entry = CurrencyAssetEntry.objects.get(id=pk)
